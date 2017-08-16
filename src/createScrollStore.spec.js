fc8f0cd88c6ed1142435757332eb23db69704c55
Michael Dougall <laheen@gmail.com>
2017-08-16 12:03:23 +1000

    chore(test): adds test

--- src/createScrollStore.spec.js
+++ src/createScrollStore.spec.js
@@ -1,3 +1,32 @@
-describe('createScrollStore', () => {
+import React from 'react';
+import PropTypes from 'prop-types';
+import { mount } from 'enzyme';
+import createScrollStore from './';
 
+describe('createScrollStore()', () => {
+  const RestoreScrollOnMount = createScrollStore();
+
+  const App = (props) => (
+    <div>
+      {props.show && <RestoreScrollOnMount />}
+    </div>
+  );
+
+  App.propTypes = {
+    show: PropTypes.bool,
+  };
+
+  it('should store scroll position on unmount and then set it on mount', () => {
+    const spy = sinon.spy(window, 'scrollTo');
+    document.body.scrollTop = 500;
+
+    const wrapper = mount(<App show />);
+    wrapper.setProps({ show: false });
+    wrapper.setProps({ show: true });
+
+
+    expect(spy).to.have.been.calledWith(0, 500);
+
+    spy.reset();
+  });
 });
